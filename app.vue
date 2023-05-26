e90eb0323ad45044f2e4bdb0068bae33daef2df4
DESKTOP-B1QD8OP\hoang <hoang.nguyen@molinetwork.com>
2023-05-26 18:22:25 +0700

    a

---
+++ app.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>test</h1>
+<!--  <router-view></router-view>-->
+</template>
---
+++ nuxt.config.js
@@ -0,0 +1,8 @@
+export default {
+  head: {
+    title: 'Nuxt + Now2'
+  },
+  serverMiddleware: [
+    '~/server-middleware/log.js'
+  ]
+}
